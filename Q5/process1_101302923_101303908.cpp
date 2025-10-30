f6cca67d7faa578bf8c230fb435480f453a35932
Keon <jevon.foster55@gmail.com>
2025-10-30 15:31:45 -0400

    Started Q5

--- Q5/process1_101302923_101303908.cpp
+++ Q5/process1_101302923_101303908.cpp
@@ -15,6 +15,21 @@
 
 int createSharedVariable(int **var, size_t size);
 
+int createSem(int init);
+
+void removeSem(int id);
+
+int signalSem(int id);
+
+int waitSem(int id, int flag);
+
+union semun {
+	int val;
+	semid_ds *buf;
+	ushort *array;
+	seminfo *__buf;
+}arg;
+
 int main(){
 	int *counter = nullptr;
 	int *multiple = nullptr;
@@ -29,6 +44,11 @@ int main(){
 	*counter = 0;
 	*multiple = 3;
 
+	int sem = createSem(1);
+	if (sem == -1) {
+		return EXIT_FAILURE;
+	}
+
 	pid_t pid = fork();
 	if (pid < 0) {
 		std::cout << "Fork failed" << std::endl;
@@ -40,20 +60,36 @@ int main(){
 	if (pid == 0) {
 		while (*counter <= 100); //Wait until count hits 100
 		while (*counter <= 500) {
+			if (waitSem(sem, 0) == -1) {
+				std::cout << "[Child] Wait Semaphore failed" << std::endl;
+				return EXIT_FAILURE;
+			}
 			if (*counter % *multiple == 0) {
 				std::cout << "[Child] Multiple: "<< *multiple << ", Count: " << *counter << std::endl;
 			}
 			*counter = *counter + 1;
 			sleep(1);
+			if (signalSem(sem) == -1) {
+				std::cout << "[Child] Signal Semaphore failed" << std::endl;
+				return EXIT_FAILURE;
+			}
 		}
 	}
 	else {
 		while (*counter <= 500) {
+			if (waitSem(sem, 0) == -1) {
+				std::cout << "[Parent] Wait Semaphore failed" << std::endl;
+				return EXIT_FAILURE;
+			}
 			if (*counter % *multiple == 0) {
 				std::cout << "[Parent] Multiple: "<< *multiple << ", Count: " << *counter << std::endl;
 			}
 			*counter = *counter + 1;
 			sleep(1);
+			if (signalSem(sem) == -1) {
+				std::cout << "[Parent] Signal Semaphore failed" << std::endl;
+				return EXIT_FAILURE;
+			}
 		}
 	}
 
@@ -87,4 +123,42 @@ int createSharedVariable(int **var, size_t size) {
 	}
 	*var = static_cast<int*>(var_addr);
 	return var_id;
+}
+
+int createSem(int init) {
+	int id;
+	semun data;
+
+	id = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0666);
+	if (id == -1) {
+		std::cout << "Get Semaphore failed" << std::endl;
+		return id;
+	}
+	data.val = init;
+	if (semctl(id, 0, SETVAL, arg) == -1) {
+		std::cout << "Init Semaphore failed" << std::endl;
+		removeSem(id);
+		return -1;
+	}
+	return id;
+}
+
+void removeSem(int id) {
+	semctl(id, 0, IPC_RMID, 0);
+}
+
+int signalSem(int id) {
+	sembuf Op;
+	Op.sem_num = 0;
+	Op.sem_op = 1;
+	Op.sem_flg = 0;
+	return semop(id, &Op, 1);
+}
+
+int waitSem(int id, int flag) {
+	sembuf Op;
+	Op.sem_num = 0;
+	Op.sem_op = -1;
+	Op.sem_flg = flag;
+	return semop(id, &Op, 1);
 }
